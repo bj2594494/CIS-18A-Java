c8eb58ed6cb98432de8d86bf2d8c0beaf4078d6e
bj2594494 <byeongju8282@gmail.com>
2018-09-12 20:00:50 -0700

    Signed-off-by: bj2594494 <byeongju8282@gmail.com>

---
+++ Class/BasicInputOutputDemo/src/bdj/BasicInputOutputDemo.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bdj;
+
+import java.util.Scanner;
+public class BasicInputOutputDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // declare integer variables
+        int value1, value2, sum, diff, product, quotnt, rmaindr;
+        
+        // create scanner object called keyboard to read in characters user types
+        Scanner keybrd = new Scanner(System.in);
+        
+        // promt the user to enter a value for value1
+        System.out.print("Enter a value for value 1: ");
+        value1 = keybrd.nextInt();
+        
+        // promt the user to enter a value for value2
+        System.out.print("Enter a value for value 2: ");
+        value2 = keybrd.nextInt();
+        
+        // let's do some basic computations
+        sum = value1+value2;
+        diff = value1-value2;
+        product = value1*value2;
+        quotnt = value1/value2;
+        rmaindr = value1%value2;
+        
+        // output our results
+        System.out.printf("The sum of %d and %d is %d.%n",value1,value2,sum);
+        System.out.printf("The difference of %d and %d is %d.%n",value1,value2,diff);
+        System.out.printf("The product of %d and %d is %d.%n",value1,value2,product);
+        System.out.printf("The quotient of %d and %d is %d.%n",value1,value2,quotnt);
+        System.out.printf("The remainder of %d and %d is %d.%n",value1,value2,rmaindr);
+        
+    }
+    
+}
